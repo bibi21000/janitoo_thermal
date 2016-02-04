89c73c172fdb718f4714d2f91f6f04286be7502b
bibi21000 <bibi21000@gmail.com>
2016-02-04 13:44:17 +0100

    Updates for docker/threads

--- tests/test_thread.py
+++ tests/test_thread.py
@@ -54,7 +54,7 @@ COMMAND_DISCOVERY = 0x5000
 assert(COMMAND_DESC[COMMAND_DISCOVERY] == 'COMMAND_DISCOVERY')
 ##############################################################
 
-JNTTThreadRun.skipDockerTest()
+#~ JNTTThreadRun.skipDockerTest()
 
 class TestThermalThread(JNTTThreadRun, JNTTThreadRunCommon):
     """Test the thread
