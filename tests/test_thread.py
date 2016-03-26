a89bd1e8b1a487a326b7e9895222614b705fda18
bibi21000 <bibi21000@gmail.com>
2016-03-26 22:51:38 +0100

    Updates for split

--- tests/test_thread.py
+++ tests/test_thread.py
@@ -45,7 +45,7 @@ from janitoo.utils import TOPIC_NODES, TOPIC_NODES_REPLY, TOPIC_NODES_REQUEST
 from janitoo.utils import TOPIC_BROADCAST_REPLY, TOPIC_BROADCAST_REQUEST
 from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_SYSTEM, TOPIC_VALUES_BASIC
 
-from janitoo.threads.remote import RemoteBus
+from janitoo_factory.threads.remote import RemoteBus
 
 class TestThermalThread(JNTTThreadRun, JNTTThreadRunCommon):
     """Test the thread
